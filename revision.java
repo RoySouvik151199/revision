36a61f79e9786478addf616ad1473deb45645994
Souvik Roy <souvikroy1511@gmail.com>
2023-04-27 14:26:46 +0530

    practicecommit

---
+++ revision.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class revision
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("ENter a number");
+		int a = sc.nextInt();
+		System.out.println("Number printed is :" + a);
+	}
+}
